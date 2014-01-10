8dbad3b6de20d9c0555589114d786bfb5fa71717
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-01-10 23:10:53 +0100

    fetch database with adb

--- src/clj_whatsapp/core.clj
+++ src/clj_whatsapp/core.clj
@@ -1,6 +1,6 @@
-(ns clj-whatsapp.core)
+(ns clj-whatsapp.core
+  (:use clj-ssh.ssh
+        [clojure.java.shell :only [sh]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;(def host "192.168.1.3")
+(sh "adb root && adb pull /data/data/com.whatsapp/databases/msgstore.d")
